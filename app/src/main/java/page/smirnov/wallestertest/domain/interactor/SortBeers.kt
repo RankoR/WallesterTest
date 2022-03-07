a9c350087c886cbde2053e1977a03fd436251373
Artem Smirnov <artem@smirnov.page>
2022-03-07 22:16:53 +0200

    Add comment for sorting

--- app/src/main/java/page/smirnov/wallestertest/domain/interactor/SortBeers.kt
+++ app/src/main/java/page/smirnov/wallestertest/domain/interactor/SortBeers.kt
@@ -9,6 +9,10 @@ interface SortBeers {
 
 class SortBeersImpl : SortBeers {
 
+    /**
+     * In fact, we can also sort in-place, but it'll require a mutable list.
+     * It would give higher performance (less GC), but it's not kotlin-idiomatic
+     */
     override fun exec(beers: List<Beer>, sortingMode: SortingMode): List<Beer> {
         return when (sortingMode) {
             // Unfortunately «when» can't be placed in sortedBy as the compiler can't resolve returning type
